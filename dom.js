57b18e8d8941b89e722f4bd65e49d1d38303c6ea
Akintunde Thomas <tsatm00@yahoo.com>
2017-10-07 18:55:04 -0400

    Mini challenge home work

---
+++ dom.js
@@ -0,0 +1,129 @@
+// Create a script with two variables, each assigned to a number. Add them together and output the result to the console. 
+// var a = 3
+// var b = 5
+// var sum = (a + b)
+
+// console.log(sum)
+
+// Now do the same with two strings.
+// var x = 'The land: '
+// var y = 'is green'
+
+// var sum = (x+y)
+
+// console.log(sum)
+
+// Create a multidimensional array related to a subject that interests you. Output two of the items in sub-arrays to the console.
+
+// var fifaRatings = [
+// 	{
+// 		name: 'Lacazette',
+// 		position: 'striker',
+// 		nationality: 'France',
+// 		pass: 87,
+// 		dribble: 85
+// 	},
+// 	{
+// 		name: 'Ozil',
+// 		position: 'Midfielder',
+// 		nationality: 'Germany',
+// 		pass: 89,
+// 		dribble: 88		
+// 	},
+// 	{
+// 		name: 'Bellerin',
+// 		position: 'Defender',
+// 		nationality: 'Spain',
+// 		pass: 69,
+// 		dribble: 78	
+// 	}
+// ];
+
+// // console.log(fifaRatings)
+
+// for(var i = 0; i < fifaRatings.length; i++){
+// 	if(fifaRatings[i].pass > 80 && fifaRatings[i].dribble > 83){
+// 		console.log(fifaRatings[i])
+// 	}
+// };
+
+// Write a script that checks if a variable is less than 100. If it is, alert the user that their variable is less than 100. 
+// If it is not alert the user of what the value was and that it was greater than 100.
+
+// var number =  prompt ("please enter a number less than 100")
+//    num(number)
+
+
+//  	function num(no) {
+//  		var no;
+ 		
+//  		if (no < 100){
+//  			alert('Good! You entered a valid number')
+//  		}
+//  		else if (no >= 100){
+//  			alert('You entered ' + no + ' and it is greater than 100')
+//  		}
+// 	}
+
+// Declare a function that takes a name as an argument and tells the user what name they've entered. Try running it after it has been declared.
+
+// var name = prompt('Please enter a name')
+// 	alert('You entered the name: ' + name)
+
+// Declare a function that depending upon which virtual 'door' was entered tells the user they've received a different 'prize' in an alert. After declaring the function, try running it with different options. There must be at least 3 doors.
+
+// var doorNumber =  prompt ("please enter a number")
+//    door(doorNumber)
+
+
+//  	function door(dn) {
+//  		var dn;
+ 		
+//  		if (dn == 1){
+//  			alert("Hooray! You've won a free cup of iced tea")
+//  		}
+//  		else if (dn == 2){
+//  			alert("Hooray! You've won a free cup of chai tea")
+//  		}
+//  		else if (dn == 3) {
+//  			alert("Hooray! You've won a free bottle of smart water")
+//  		}
+// }
+
+
+// Create an HTML page with two buttons that argue with each other. When one button is clicked, the text "I'm right" should be placed next to it. When the other button is clicked, the text is replaced with, "No, I'm right!"
+
+// document.getElementById('click1').onclick = function(){
+// 	document.getElementById('click1').innerText = ("I'm right")
+// }
+
+// Create an HTML page with a large element on the page that says "Don't hover over me" inside of it. When you hover over the element, send an alert to the user that says, "Hey, I told you not to hover over me!"
+
+// document.getElementById('click2').onclick = function(){
+// 	document.getElementById('click2').innerText = ("No! I'm right")
+// }
+
+// // 
+
+// document.getElementsByTagName('h1')[0].onmouseover = function(){
+// 	alert('Hey, I told you not to hover over me!')
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
